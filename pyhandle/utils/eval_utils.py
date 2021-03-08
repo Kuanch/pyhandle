8e82bb125fe85f909d04c027b3012b049228e860
sixigma <Sixigma6@gmail.com>
2021-03-08 20:31:02 +0800

    fix: remove read_*

--- pyhandle/utils/eval_utils.py
+++ pyhandle/utils/eval_utils.py
@@ -8,6 +8,7 @@ from utils.matplot_utils import plot_classes_preds
 def eval_for_one_epoch(dataset, model, total_step=10):
     eval_result = {}
 
+    model.eval()
     with torch.no_grad():
         eval_result['pred_figs'] = pred_and_draw_image(model, dataset)
         eval_result['precision'], eval_result['recall'] = metric_eval(total_step, model, dataset)
@@ -29,19 +30,20 @@ def images_to_probs(net, images):
 
 
 def pred_and_draw_image(model, dataset):
-    images, labels = dataset.read_test()
-    preds, probs = images_to_probs(model, images)
-    return plot_classes_preds(preds, probs, images, labels)
+    loader = dataset.test_loader
+    images, labels = next(iter(loader))
+    preds, probs = images_to_probs(model, images.cuda())
+    return plot_classes_preds(preds, probs, images.cuda(), labels.cuda())
 
 
 def metric_eval(total_step, model, dataset):
     TP = FP = TN = FN = 0
-    for step in range(total_step):
-        images, labels = dataset.read_test()
-        preds = model(images)
+    loader = dataset.test_loader
+    for images, labels in loader:
+        preds = model(images.cuda())
         _, class_preds_batch = torch.max(preds, 1)
 
-        np_labels = labels.cpu().numpy()
+        np_labels = labels.numpy()
         np_preds = class_preds_batch.cpu().numpy()
 
         TP += sum(np_labels[np.where(np_preds == 1)[0]] == 1)
