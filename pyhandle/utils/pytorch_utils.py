19bd433be0658773769dde4b5e910ef3538e7d74
sixigma <Sixigma6@gmail.com>
2021-03-11 16:38:29 +0800

    feat: save preprocess obj with model

--- pyhandle/utils/pytorch_utils.py
+++ pyhandle/utils/pytorch_utils.py
@@ -2,11 +2,12 @@ import os
 import torch
 
 
-def save_model(model_state, optim_state, loss, epoch, path):
+def save_model(model_state, optim_state, preprocessor, loss, epoch, path):
     dir_path = os.path.dirname(path)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     torch.save({'epoch': epoch,
                 'model_state_dict': model_state,
                 'optim_state_dict': optim_state,
+                'preprocessor': preprocessor,
                 'loss': loss}, path)
